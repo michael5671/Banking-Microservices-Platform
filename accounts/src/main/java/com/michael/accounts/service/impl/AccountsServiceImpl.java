d7c34b8dc27e3c00c677a815a5e5e341b6b75c36
michael5671 <nq2019.nguyengiaphu190304@gmail.com>
2025-11-28 22:49:55 +0700

    feat: Add Accounts Service

--- accounts/src/main/java/com/michael/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/michael/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,17 @@
 package com.michael.accounts.service.impl;
 
+import com.michael.accounts.constants.AccountsConstants;
 import com.michael.accounts.dto.CustomerDto;
+import com.michael.accounts.entity.Accounts;
+import com.michael.accounts.entity.Customer;
+import com.michael.accounts.mapper.CustomerMapper;
 import com.michael.accounts.repository.AccountsRepository;
 import com.michael.accounts.repository.CustomerRepository;
 import com.michael.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +24,16 @@ public class AccountsServiceImpl implements IAccountsService {
      */
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Customer savedCustomer = customerRepository.save(customer);
+    }
+    private Accounts createNewAccounts(Customer customer){
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
 
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
     }
 }
