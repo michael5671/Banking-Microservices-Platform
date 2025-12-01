8a87c3562da154cbff153765b3f10b12cef3bea3
michael5671 <nq2019.nguyengiaphu190304@gmail.com>
2025-12-01 19:57:22 +0700

    feat(account): add auditing

--- accounts/src/main/java/com/michael/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/michael/accounts/service/impl/AccountsServiceImpl.java
@@ -36,8 +36,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber"
                     + customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccounts(savedCustomer));
     }
@@ -50,8 +48,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
         newAccount.setAccountNumber(randomAccNumber);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
