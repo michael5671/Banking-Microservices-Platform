b4575101b7c8d74c0b31efe58125c23b3c724a0e
michael5671 <nq2019.nguyengiaphu190304@gmail.com>
2025-12-02 01:29:17 +0700

    feat(account): enhancing documentation of REST APIs

--- accounts/src/main/java/com/michael/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/michael/accounts/AccountsApplication.java
@@ -1,11 +1,34 @@
 package com.michael.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info=@Info(
+                title="Accounts microservice REST API Documentation",
+                description = "Banking Platform Account microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name="Gia Phu",
+                        email="phu@giaphu.com"
+                ),
+                license = @License(
+                        name="Apache 20",
+                        url=""
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Bank platform Account microservice REST API Documentation"
+        )
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
