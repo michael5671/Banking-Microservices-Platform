2f4138554484d272eb451b9dce30ae7136be2e7b
michael5671 <nq2019.nguyengiaphu190304@gmail.com>
2025-12-01 16:09:37 +0700

    feat(account): add input validation

--- accounts/src/main/java/com/michael/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/michael/accounts/dto/AccountsDto.java
@@ -1,13 +1,19 @@
 package com.michael.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message ="AccountNumber cannot be null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account type cannot be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "branch Address cannot be null or empty")
     private String branchAddress;
 }
